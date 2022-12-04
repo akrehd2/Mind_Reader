a39ce25b87fbcb08ef082814931a89ff7a6d8571
dejujak <dejujak0808@naver.com>
2022-12-04 19:13:24 +0900

    error
    error

---
+++ Assets/Scripts/OtherSkill.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherSkill : MonoBehaviour
+{
+    [SerializeField] int skillNumber;
+    public List<int> noUseNumber = new List<int>()
+    { 1,2,3,4,5};
+    public int nUNSize = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void AISkill()
+    {
+        if(Count.otherNumberCount==1)
+        {
+
+        }
+    }
+}
